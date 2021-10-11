6ffe059eb9ede52e23b2a080aad490426df74113
hydrz <n.haoyuan@gmail.com>
2021-10-11 13:02:44 +0800

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,30 @@
+## 0.7.0
+
+* Support rem and em unit.
+
+## 0.5.3
+
+* upgrade kraken to 0.7.3+1
+## 0.5.1
+
+* default display set to block.
+
+## 0.5.0
+
+* Change `_propertyChangedListener` to `void Function(String, String, String)`.
+
+## 0.4.0
+
+* upgrade to kraken 0.7.0
+
+## 0.2.3
+
+* Remove WidgetsFlutterBinding.ensureInitialized() when initialized;
+
+## 0.0.2
+
+* Migrate from kraken.
+
+## 0.0.1
+
+* Initial release.
---
+++ lib/kraken_webview.dart
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2019-present Alibaba Inc. All rights reserved.
+ * Author: Kraken Team.
+ */
+
+import 'dart:ffi';
+import 'platform.dart';
+import 'package:kraken/dom.dart';
+import 'iframe_element.dart';
+
+typedef Native_InitBridge = Void Function();
+typedef Dart_InitBridge = void Function();
+
+final Dart_InitBridge _initBridge = nativeDynamicLibrary
+    .lookup<NativeFunction<Native_InitBridge>>('initBridge')
+    .asFunction();
+
+void initBridge() {
+  _initBridge();
+}
+
+class KrakenWebView {
+  static void initialize() {
+    initBridge();
+    ElementManager.defineElement('IFRAME', (id, nativePtr, elementManager) {
+      return IFrameElement(
+          id, nativePtr.cast<NativeIframeElement>(), elementManager);
+    });
+  }
+}
